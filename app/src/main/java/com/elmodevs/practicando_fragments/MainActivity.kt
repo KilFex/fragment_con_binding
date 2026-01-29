baf76312bd2c093d498d4c5cbc7746fbd431c32a
KilFex <elivan.1251996@gmail.com>
2026-01-29 00:28:04 -0500

    configurando mainactivity

--- app/src/main/java/com/elmodevs/practicando_fragments/MainActivity.kt
+++ app/src/main/java/com/elmodevs/practicando_fragments/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fragment_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.f_home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
